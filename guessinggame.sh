94f51bc6e10e52b530e2d76f0aa617675de1f0ac
chanakyasethi <36638966+chanakyasethi@users.noreply.github.com>
2023-07-04 11:36:10 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+echo -e "Guess number of files in the current directory. \n"
+shopt -s nullglob
+numfiles=(*)
+numfiles=${#numfiles[@]}
+
+
+function game {
+while (( guess != $1 )); do
+	read -p "Enter guess : " guess
+	if (( guess < $1 )); then
+		echo "Too low"
+	elif (( guess > $1 )); then
+		echo "Too high"
+	fi
+done
+echo -e "Correct! Thank you for playing.\n"
+}
+
+game $numfiles
