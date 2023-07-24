a889159b6fec54d02fee7b16a882f86c3846701e
chanakyasethi <36638966+chanakyasethi@users.noreply.github.com>
2023-07-24 10:57:24 +0530

    hehe

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+echo -e "Please enter number of files in the current directory. \n"
+read -p answer
+
 echo -e "Guess number of files in the current directory. \n"
 shopt -s nullglob
 numfiles=(*)
@@ -7,15 +10,22 @@ numfiles=${#numfiles[@]}
 
 
 function game {
+
 while (( guess != $1 )); do
+
 	read -p "Enter guess : " guess
+	
 	if (( guess < $1 )); then
 		echo "Too low"
+		
 	elif (( guess > $1 )); then
 		echo "Too high"
+		
 	fi
 done
+
 echo -e "Correct! Thank you for playing.\n"
+
 }
 
 game $numfiles
